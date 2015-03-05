0ca6477086244e3cf5fa307887227d9d79e27b58
Kendra <kdodds@gmail.com>
2015-03-05 15:47:29 -0700

    setup user db and rendering json

--- rantly-api/db/seeds.rb
+++ rantly-api/db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+User.delete_all
+
+User.create!(
+  first_name: "Admin",
+  last_name: "User",
+  email: "admin@example.com",
+  password: "password"
+)
+
+10.times do |i|
+  User.create!(
+  first_name: Faker::Name.first_name,
+  last_name: Faker::Name.last_name,
+  email: Faker::Internet.email,
+  password: Faker::Lorem.word
+  )
+
+end
