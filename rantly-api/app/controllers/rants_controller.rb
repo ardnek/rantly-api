63537aa0297b8c15f20e7222e90a8e7806b609c2
Kendra <kdodds@gmail.com>
2015-03-06 18:36:12 -0700

    finished rants_controller

--- rantly-api/app/controllers/rants_controller.rb
+++ rantly-api/app/controllers/rants_controller.rb
@@ -1,27 +1,28 @@
 class RantsController < ApplicationController
 
-  before_action :set_user
+  # before_action :set_user
 
   def index
     @rants = Rant.all
     render json: @rants
   end
 
-  def new
-    @rant = Rant.new
-    render json: @rant
-  end
+  # def new
+  #   @rant = Rant.new
+  #   render json: @rant
+  # end
 
   def update
-    @rant = Rant.find(rant_params)
+    @rant = Rant.find(params[:id])
     @rant.update(rant_params)
-    redirect_to users_path
+    render json: @rant
   end
 
   def create
     @rant = Rant.new(rant_params)
     @rant.save
-    redirect_to users_path
+    render json: @rant
+    # render(:json => @rant)
     # @rant = @user.rants(rant_params)
   end
 
@@ -30,18 +31,24 @@ class RantsController < ApplicationController
     render json: @rant
   end
 
-  def edit
+  def destroy
     @rant = Rant.find(params[:id])
-    if @rant.save
-    else
-      render json: @rant
+    @rant.destroy
   end
+
+
+  # def edit
+  #   @rant = Rant.find(params[:id])
+  #   if @rant.save
+  #   else
+  #     render json: @rant
+  # end
 
   private
 
-  def set_user
-    @user = User.find(params[:id])
-  end
+  # def set_user
+  #   @user = User.find(params[:id])
+  # end
 
   def rant_params
     params.require(:rant).permit(:title, :body, :user_id)
--- rantly-api/app/controllers/users_controller.rb
+++ rantly-api/app/controllers/users_controller.rb
@@ -1,37 +1,44 @@
 class UsersController < ApplicationController
 
   def index
-    @users = User.all
-    render json: @users
+    @users = User.all # set instance variable to equal all the rants
+    render json: @users # outputs the @user variable as valid JSON
   end
 
   def show
-    @user = User.find(params[:id])
+    @user = User.find(params[:id]) # set @user to 1 specific user returning the value of params[:id]
     render json: @user
   end
 
   def create
     @user = User.new(user_params)
-    @user.save
+    # creates a new instance of the User
+    # class, then passes the user_params to the new instance.
+    # This has NOT been saved yet, only setup
+    @user.save # adds the new user to the DB -- just a method getting called on variable
     render json: @user
   end
 
   def update
     @user = User.find(params[:id])
-    @user.update(user_params)
+    @user.update(user_params) # calls the update method on the @user, update the information on the DB
+    #and saves it using the params that were passed in as an argument()
     render json: @user
   end
 
   def destroy
     @user = User.find(params[:id])
-    @user.destroy
-    @users = User.all
+    @user.destroy # calls the destroy method on the @user. removes @user from the DB.
   end
 
   private
 
   def user_params
     params.require(:user).permit(:first_name, :last_name, :email, :password)
+    # params is a hash
+    # .require(:user). I only want the :user key coming out of params
+    # filtering down to the specific hash we want
+    # .permit(...) allows only the specific columns to be changed that are listed
   end
 
 end
